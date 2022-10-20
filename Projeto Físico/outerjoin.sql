c2dc70a6502ece7b542a3f3f7d50ee6cd4baa816
Nathalia B <74332487+nathaliafab@users.noreply.github.com>
2022-10-19 21:56:11 -0300

    Reformulação da segunda consulta

--- Projeto Físico/outerjoin.sql
+++ Projeto Físico/outerjoin.sql
@@ -1,14 +1,13 @@
---projetar [nome] e [cpf do mentor] dos funcionários que possuem mentor, mas não fizeram nenhuma viagem
+--  projetar [nome] e [cpf do mentor] dos funcionários que possuem mentor, mas não fizeram nenhuma viagem
 SELECT F.NOME AS FUNCIONÁRIO, F.MENTOR AS CPF_MENTOR
 FROM FUNCIONARIO F LEFT OUTER JOIN VIAGEM V
 ON F.CPF = V.FUNCIONARIO
 WHERE V.FUNCIONARIO IS NULL AND F.MENTOR IS NOT NULL;
 
---projetar [destino], [funcionário] e [cliente] envolvidos numa viagem, incluindo os clientes e funcionários não envolvidos em viagem alguma
+--  projetar destino, funcionário (nome) e cliente (nome) envolvidos numa passagem,
+--  incluindo os clientes e funcionários que não compraram/venderam passagens.
 SELECT P.LOCAL_DESTINO AS DESTINO, F.NOME AS FUNCIONÁRIO, C.NOME AS CLIENTE
-FROM VIAGEM V INNER JOIN PASSAGEM P
-ON V.CODIGO_PASSAGEM = P.CODIGO_RESERVA
-RIGHT OUTER JOIN CLIENTE C
+FROM PASSAGEM P RIGHT OUTER JOIN CLIENTE C
 ON C.CPF = P.CLIENTE
 FULL OUTER JOIN FUNCIONARIO F
-ON F.CPF = V.FUNCIONARIO;
+ON F.CPF = P.FUNCIONARIO;
