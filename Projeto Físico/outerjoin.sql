74d30af9f3729d06d17bdc53d326ff7b05e3e45d
Nathalia B <74332487+nathaliafab@users.noreply.github.com>
2022-10-22 18:52:28 -0300

    Update outerjoin.sql

--- Projeto Físico/outerjoin.sql
+++ Projeto Físico/outerjoin.sql
@@ -1,10 +1,4 @@
---  projetar [nome] e [cpf do mentor] dos funcionários que possuem mentor, mas não fizeram nenhuma viagem
-SELECT F.NOME AS FUNCIONÁRIO, F.MENTOR AS CPF_MENTOR
-FROM FUNCIONARIO F LEFT OUTER JOIN VIAGEM V
-ON F.CPF = V.FUNCIONARIO
-WHERE V.FUNCIONARIO IS NULL AND F.MENTOR IS NOT NULL;
-
---  projetar destino, funcionário (nome) e cliente (nome) envolvidos numa passagem,
+--  projetar [destino], [funcionário] e [cliente] envolvidos numa passagem,
 --  incluindo os clientes e funcionários que não compraram/venderam passagens.
 SELECT P.LOCAL_DESTINO AS DESTINO, F.NOME AS FUNCIONÁRIO, C.NOME AS CLIENTE
 FROM PASSAGEM P RIGHT OUTER JOIN CLIENTE C
