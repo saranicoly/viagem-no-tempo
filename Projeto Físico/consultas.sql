0ef0263458d2d9f1973fd916884444508812d3f9
Sara Lins <sara.lins@luizalabs.com>
2022-10-22 18:37:35 -0300

    chore: change query

--- Projeto Físico/consultas.sql
+++ Projeto Físico/consultas.sql
@@ -1,6 +1,6 @@
--- quantidade de clientes que compraram passagem para cada destino num determinado ano
-SELECT LOCAL_DESTINO, extract(year from DATA_DESTINO), COUNT(*) FROM PASSAGEM
--- em todos os anos
-GROUP BY LOCAL_DESTINO, extract(year from DATA_DESTINO)
--- em determinado ano
-HAVING extract(year from DATA_DESTINO)=1856;
+-- Projetar a quantidade de passagens num determinado ano quando houvesse mais do que duas passagens
+SELECT LOCAL_DESTINO, COUNT(*)
+FROM PASSAGEM
+WHERE extract(year from DATA_DESTINO)=1856
+GROUP BY LOCAL_DESTINO
+HAVING COUNT(*)>=2;
