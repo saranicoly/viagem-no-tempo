fb2f106da7d46894583264453968f9d82a5794bc
Mavi <soaresmunizm@gmail.com>
2022-10-22 17:21:13 -0300

    (feat): add antijoin with NOT EXISTS

--- Projeto Físico/antijoin.sql
+++ Projeto Físico/antijoin.sql
@@ -1,7 +1,16 @@
---Projetar o código do setor e o nome dos funcionário que não realizaram nenhuma missão
+--Projetar o código do setor e o cpf dos funcionário que não realizaram nenhuma missão com NOT IN
 
  SELECT S.CODIGO, S.CPF
  FROM SETOR S
  WHERE S.CPF NOT IN
   (SELECT P.FUNCIONARIO
   FROM PARTICIPA P);
+
+--Projetar o código do setor e o cpf dos funcionário que não realizaram nenhuma missão com NOT EXISTS
+
+SELECT S.CODIGO, S.CPF
+FROM SETOR S
+WHERE NOT EXISTS
+ ( SELECT P.FUNCIONARIO
+    FROM PARTICIPA P
+    WHERE P.FUNCIONARIO = S.CPF);
